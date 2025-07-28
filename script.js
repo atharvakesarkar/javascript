acf1ba8bf9dd66b98cfae8019ef762db9f52d601
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-29 01:42:38 +0530

    Update script.js
    todays js learning i have learn about the template literals , operators like arthemtic operator , unary opertor , logical opertor , comparison opertaor

--- script.js
+++ script.js
@@ -1,4 +1,86 @@
-let PencilPrize = 5;
-let ErasorPrize = 5;
+// let PencilPrize = 5;
+// let ErasorPrize = 5;
 
-console.log("the total price is : ", PencilPrize + ErasorPrize ," ruppess")
+// console.log("the total price is : ", PencilPrize + ErasorPrize ," ruppess");
+
+// using the string:
+// let output = "The total price is: " + (PencilPrize + ErasorPrize) + " ruppess";
+// console.log(output);
+
+
+
+// use of Template literals:
+// let output = `The total price is: ${PencilPrize + ErasorPrize} ruppess.`;  //we use backtik for the template literals and in curly braces you can use varibles
+// console.log(output);
+
+
+
+// Artihmetic operator 
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log (num1 + num2);   //addition
+// console.log (num1 - num2);   // subtract
+// console.log (num1 * num2);   //multiplication
+// console.log (num1 / num2);   // divide
+// console.log (num1 % num2);   // modolus
+// console.log (num1 ** num2);  // power
+
+
+
+// Unary operator:
+// let num1 = 10;
+// console.log(num1++); //10
+// console.log(++num1); //12
+
+// console.log(num1--); //10
+// console.log(--num1); //8
+
+
+
+// comparison operator
+
+// let age = 10;
+// let voting_age = 18;
+// let output = age == voting_age; //equal than
+// console.log(output);
+
+// let output = age > voting_age; //greater than
+// console.log(output);
+
+// let output = age >= voting_age; //greater than equal total
+// console.log(output);
+
+// let output = age < voting_age; //smaller than
+// console.log(output);
+
+// let output = age <= voting_age; //smaller than equal total
+// console.log(output);
+
+// let output = age != voting_age; // not equal
+// console.log(output);
+
+
+// comparing number and string , javascript ignore the datatype and focus on the value & same goes for not equal 
+// let num = 10;
+// let str = '10';
+// console.log(num == str);
+
+
+// Triple equal to - it compares the both datatype and value.
+// let num = 10;
+// let str = '10';
+// console.log(num === str); //false
+
+
+
+
+// comparison for non-numbers:
+// 1. all the numbers has his unicode , so which unicode is higher he will be the greater 
+// let a = 'p'; //1170
+// let b = 'P'; //1150
+// console.log(a>b);
+
+
+
+
