88a4e440bb4fe84df06bb5b6ca007ee198a234f5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-30 00:51:41 +0530

    Update conditon.js

--- conditonal_Statements/conditon.js
+++ conditonal_Statements/conditon.js
@@ -19,3 +19,19 @@
 // if (age < 18){
 //     console.log("No, you can't vote.")
 // }
+
+
+// pratice Qs
+
+// let color = "green";
+
+
+// if (color == "red"){
+//     console.log("stop your vehicle.");
+// }
+// if (color == "yellow"){
+//     console.log("be ready.");
+// }
+// if (color == "green"){
+//     console.log("go....");
+// }
