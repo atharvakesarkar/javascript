fd887e031a379ba0105db2647a6d12a1a2ffbc1a
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-30 00:36:58 +0530

    Add files via upload

---
+++ conditonal_Statements/conditon.js
@@ -0,0 +1,21 @@
+// let age = 18;
+
+// if (age>=18){
+//     console.log("yes, you are eligible for vote.");
+// }
+// else {
+//     console.log("sorry, you are not eligible.");
+// }
+
+
+
+// using nested if
+
+// let age = 10;
+
+// if (age >= 18){
+//     console.log("yes, you can vote.")
+// }
+// if (age < 18){
+//     console.log("No, you can't vote.")
+// }
