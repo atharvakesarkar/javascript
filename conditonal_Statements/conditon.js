3a63117c76c8c35a913c7019ab67855a7aad0477
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-07-31 01:26:13 +0530

    Update conditon.js
    added the else statement

--- conditonal_Statements/conditon.js
+++ conditonal_Statements/conditon.js
@@ -37,40 +37,50 @@
 // }
 
 
-// use of else if statement
-let age = 20;
+// // use of else if statement
+// let age = 20;
 
-if (age >= 18){
-    console.log("you are eligible for license.")
-}
-else if (age >= 21){
-    console.log("you are elgible for marriage.")
-}
-else {
-    console.log("you are not elgible")
-}
+// if (age >= 18){
+//     console.log("you are eligible for license.")
+// }
+// else if (age >= 21){
+//     console.log("you are elgible for marriage.")
+// }
+// else {
+//     console.log("you are not elgible")
+// }
 
 
-// marks example for the else if statement(Multiple if else statement)
-let marks = 50;
+// // marks example for the else if statement(Multiple if else statement)
+// let marks = 50;
 
-if (marks >= 90){
-    console.log("A+");
-}
-else if (marks >=80){
-    console.log("A");
-}
-else if (marks >=70){
-    console.log("B+");
-}
-else if (marks >=60){
-    console.log("B");
-}
-else if (marks >=50){
-    console.log("C");
-}
-else if (marks >=40){
-    console.log("Fail");
-}
+// if (marks >= 90){
+//     console.log("A+");
+// }
+// else if (marks >=80){
+//     console.log("A");
+// }
+// else if (marks >=70){
+//     console.log("B+");
+// }
+// else if (marks >=60){
+//     console.log("B");
+// }
+// else if (marks >=50){
+//     console.log("C");
+// }
+// else if (marks >=40){
+//     console.log("Fail");
+// }
 
+
+// use of else statement
+// let age = 8;
+
+// if (age >= 18){
+//     console.log("yes you can vote.")
+// }
+// else {
+//     console.log("your age is bellow 18..")
+// }
 
