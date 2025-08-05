62147550af1678cd5e441a76b38ef08821fa2acc
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-06 00:41:31 +0530

    Add files via upload

---
+++ logical opertor/logic.js
@@ -0,0 +1,14 @@
+// && opertor is use to combine 2 experssion
+
+// true && true - true
+// true && false  - false
+// false && true - false
+// false && false - false
+
+// console.timeLog((5>3) && (5>3))
+// console.timeLog((5>3) && (5<3))
+// console.timeLog((5<3) && (5>3))
+// console.timeLog((5<3) && (5<3))
+
+
+// read world example
