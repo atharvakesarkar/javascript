22ca2cf26c2e19652068231eac88970e395f7b02
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-06 21:37:33 +0530

    Add files via upload

---
+++ truthly_falsy/trutly_falsy.js
@@ -0,0 +1,32 @@
+
+
+// everything in java has , its boolean value either true or false , 
+// if (true){
+//     console.log("The value is true");
+// }else {
+//     console.log("The value is false");
+// }
+
+
+
+
+// if (false){
+//     console.log("this is true value");
+// } else {
+//     console.log("this is false value");
+// }
+
+
+
+// if (0){
+//     console.log("this is true value");
+// } else {
+//     console.log("this is false value");
+// }
+
+
+// if (" "){
+//     console.log("this is true value");
+// } else {
+//     console.log("this is false value");
+// }
