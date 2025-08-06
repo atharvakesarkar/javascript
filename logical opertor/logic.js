dab224b2e26fd3790eb459e0df57415d9f15ac37
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-06 08:12:06 +0530

    Update logic.js

--- logical opertor/logic.js
+++ logical opertor/logic.js
@@ -54,3 +54,23 @@
 
 
 
+// let string = "atharva";
+
+// // console.log(string[0])
+
+// if (string.length > 3 && string[0] === "a"){
+//     console.log("this is good string");
+// } else {
+//     console.log("this is not good string");
+// }
+
+
+// q2
+// let num = 12;
+
+// if ((num%3 === 0) && ((num+1 == 15) || (num-1 == 11))) {
+//     console.log("safe")
+// } else {
+//     console.log("unsafe")
+// }
+
