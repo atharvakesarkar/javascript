8cdb670f4ddb6a3cdace7dfb98c583c1e5bd33a5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-06 21:53:17 +0530

    Update trutly_falsy.js

--- truthly_falsy/trutly_falsy.js
+++ truthly_falsy/trutly_falsy.js
@@ -1,6 +1,11 @@
 
 
-// everything in java has , its boolean value either true or false , 
+// everything in java has , its boolean value either true or false 
+// false , 0, -0 , on(bigint) , " "(empty String) , null , undefined , NaN ------------ "all this are default false value" 
+// except this all are true
+
+
+
 // if (true){
 //     console.log("The value is true");
 // }else {
@@ -30,3 +35,21 @@
 // } else {
 //     console.log("this is false value");
 // }
+
+
+
+
+// THE EMPTY STRING HAS FALSE VALUE AND THE STRING WITH THE SPACE IS TRUE VALUE:
+
+if (""){
+    console.log("this is true value");
+} else {
+    console.log("this is false value");
+}
+
+
+if (" "){
+    console.log("this is true value");
+} else {
+    console.log("this is false value");
+}
