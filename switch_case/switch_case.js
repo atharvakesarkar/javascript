152d6694e04f2b4409ba9ad1e7afa95b59f86340
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-06 22:28:45 +0530

    Add files via upload

---
+++ switch_case/switch_case.js
@@ -0,0 +1,22 @@
+
+// use when we have same fixed values that we need to compare to
+
+
+// color = "blue";
+
+// switch(color) { 
+//     case "red":
+//         console.log("stop");
+//         break;  //break statement is very important , if the break statment is not there all after statment will run
+    
+//     case "green":
+//         console.log("go!");
+//         break;
+
+//     case "yellow":
+//         console.log("ready")
+//         break;
+
+//     default:
+//         console.log("broken light.")
+// }  
