3a19a7542b6fd349b225646646791feec0459fa6
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-12 08:30:53 +0530

    Add files via upload

---
+++ alert_and_promt/pratice_js2.js
@@ -0,0 +1,68 @@
+// q1
+
+// let num = 10;
+
+// if (num % 10 == 0){
+//     console.log("good");
+// } else {
+//     console.log("bad");
+// }
+
+
+// q2
+
+// let name = prompt("enter your name: ");
+// let age = prompt("enter your age: ");
+
+// let answer = alert(` ${name} is ${age} years old.`);
+
+
+// q3
+
+// let month = 4
+
+// switch(month){
+//     case 1:
+//         console.log("January" , "February" , "March");
+//         break;
+
+//     case 2:
+//         console.log("April" , "May" , "June");
+//         break;
+
+//     case 3:
+//         console.log("July" , "August" , "September");
+//         break;
+
+//     case 4:
+//         console.log("October" , "November" , "December"); 
+//         break;
+
+//     default:
+//         console.log("invalid input...")
+// }
+
+
+// // q4
+
+// let input = "apple"
+
+// if ((input[0] == "A" || input[0] == "a") && input.length > 5){
+//     console.log("good string")
+// } else{
+//     console.log("bad string")
+// }
+
+
+
+// let num1 = 50;
+// let num2 = 30;
+// let num3 = 40;
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log("num1 is the largest number");
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log("num2 is the largest number");
+// } else {
+//     console.log("num3 is the largest number");
+// }
