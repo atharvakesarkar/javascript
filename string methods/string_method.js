b7cf22bace08cb10d7b9cf399192abcd0029e77b
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-12 08:57:09 +0530

    Add files via upload

---
+++ string methods/string_method.js
@@ -0,0 +1,10 @@
+// syntax
+// stringname.method()
+
+
+// 1.TRIM METHOD - remove all the white spaces from start and end , not in the middle of the string
+
+let str = "     hello      ";
+console.log(str.trim());
+
+
