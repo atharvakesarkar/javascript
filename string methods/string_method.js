99081a33c5f37a081d910b69f73f0e00f91a248e
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-12 09:02:01 +0530

    Update string_method.js

--- string methods/string_method.js
+++ string methods/string_method.js
@@ -4,7 +4,17 @@
 
 // 1.TRIM METHOD - remove all the white spaces from start and end , not in the middle of the string
 
-let str = "     hello      ";
-console.log(str.trim());
+// let str = "     hello      ";
+// console.log(str.trim());
+
+
+// PASSSWORD VERSION:
+
+// let password = prompt("enter your password: ");
+// let newpassword = password.trim();
+// console.log(newpassword);
+
+
+
 
 
