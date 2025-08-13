835a4f10a8b521509a08d8e9486bce06f15c25a8
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-13 08:28:33 +0530

    Add files via upload

---
+++ upper_lowecase/upper_lower.js
@@ -0,0 +1,13 @@
+// always remember , orginal string is immutable , that is why the orginal string cannot be change
+
+
+// let msg = "atharva";
+
+// let output = msg.toUpperCase();
+// console.log(output);
+
+
+// let x = "KRUTIKA";
+
+// let output = x.toLowerCase();
+// console.log(output)
