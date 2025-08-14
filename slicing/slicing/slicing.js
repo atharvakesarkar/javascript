47a972ab324177ed3b19300857034425cb6258e5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-14 07:01:30 +0530

    Add files via upload

---
+++ slicing/slicing/slicing.js
@@ -0,0 +1,13 @@
+// let a = "Atharva kesarkar";
+
+// console.log(a.slice(0,7));
+// console.log(a.slice(0));
+// console.log(a.slice(0 ,  a.length));
+
+
+
+// // if i given a minus value then it will minus that with the total length and will return 
+
+// let msg = "Atharva kesarkar";
+
+// console.log(msg.slice(-2));
