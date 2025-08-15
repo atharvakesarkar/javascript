d49bcc0f33b1fc458fe9cfee6f57171b17107a71
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-15 13:19:43 +0530

    Add files via upload

---
+++ replace _and_repeat/praticeqs.js
@@ -0,0 +1,27 @@
+// let msg = "help!";
+
+// console.log(msg.trim().toUpperCase());
+
+
+
+// q2
+let name = "Apnacollege";
+
+// console.log(name.slice(4,9));
+
+// console.log(name.indexof("na"));
+
+// console.log(name.replace("Apna","Our"));
+
+
+
+
+
+// SEPERATE THE COLLEGE PART IN ABOVE STRING AND REPLACE L WITH T
+let str2 = name.slice(4);
+let newstr = str2.replace("l","t").replace("l","t");
+console.log(newstr);
+
+
+
+
---
+++ replace _and_repeat/replace_repeat.js
@@ -0,0 +1,23 @@
+// to replace the word we use the slice method
+// syntax: str.replace(orginal value , replace value);
+
+
+
+// let str= "Atharva kesarkar";
+// console.log(str.replace("Atharva"  , "krutika"));
+
+
+// it will follow the first occurence 
+// let str = "love love love love love "
+// console.log(str.replace("love", "Atharva"));
+
+
+
+
+
+// Repeat methond will repeat the values number of time you tell them to repeat....
+
+
+// let msg = "i love to do coding ";
+
+// console.log(msg.repeat(10));
