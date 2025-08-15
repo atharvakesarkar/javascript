637855723386c9a6612ca650528f15cea980d6a4
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-16 01:51:41 +0530

    Add files via upload

---
+++ solving_questions.js
@@ -0,0 +1,78 @@
+// let name = prompt("what is your name?")
+// let age = prompt("what is your age?")
+
+
+// console.log(`Hello ${name}, your ${age} years old.`)
+
+
+// num = prompt("enter the number: ");
+
+// if (num % 2 == 0){
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+
+// let age = prompt("enter your age: ");
+// let license = prompt("you have license [yes or not]: ").toLowerCase();
+
+// if (age >= 18 && license === "yes"){
+//     console.log("yes , you can drive");
+// } else {
+//     console.log("you are not eligible for it..")
+// }
+
+
+
+
+
+// let str = "            Atharva                 "
+// console.log(str.trim().toUpperCase().replaceAll("A" ,"@"))
+
+
+
+// let str = "I love javascript ";
+
+// console.log(str.replaceAll(" ","-").repeat(3));
+
+
+
+// let day = Number (prompt("enter the weekday: ")) ;
+
+// switch (day) {
+//     case  1:
+//         console.log("Monday");
+//         break;
+    
+//     case  2:
+//         console.log("Tuesday");
+//         break;
+        
+//     case  3:
+//         console.log("Wednesday");
+//         break;
+
+//     case  4:
+//         console.log("Thursday");
+//         break;
+
+//     case  5:
+//         console.log("Friday");
+//         break;
+
+//     case  6:
+//         console.log("Saturday");
+//         break;
+
+//     case  7:
+//         console.log("Sunday");
+//         break;
+// }
+
+
+
+let name = prompt("enter the name: ");
+
+console.log(name[0].toUpperCase
+    ());
