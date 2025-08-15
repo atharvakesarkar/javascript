455d28b147469df5f3d4d0eedef455dcf1d6c72d
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-16 01:55:23 +0530

    Add files via upload
    just added

---
+++ Array/viusalizing_Array/visualize_array.js
@@ -0,0 +1,7 @@
+// linear collection of things
+// array is object , even when you fo 'type of nums" it will be object
+
+let nums = [1,2,3,4,5,5,6];
+console.log(nums[3]);
+console.log(nums.length);
+console.log(typeof nums);
