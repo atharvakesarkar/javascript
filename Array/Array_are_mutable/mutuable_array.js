438b6fa1d5f1cf100ef3dd79ea37927e2576271f
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-20 18:37:10 +0530

    Add files via upload

---
+++ Array/Array_are_mutable/mutuable_array.js
@@ -0,0 +1,15 @@
+// WE ALL KNOW THAT STRING ARE IMMUTUALBE [NO CHANGE] BUT OUR ARRAY ARE MUTUABLE [CHANGEABLE]
+
+// let arr = ["banana", "apple", "orange", "grapes" ];
+// arr[0] = "pineapple";
+// console.log(arr);
+
+
+// WE CAN ADD THE ELEMENT IN THE NON EXISTING INDEX
+
+// let arr = [10,20,40,50,50];
+// console.log(arr);
+// arr[10] = 100;
+// console.log(arr);     // OUTPUT - (11) [10, 20, 40, 50, 50, empty × 5, 100]
+
+
