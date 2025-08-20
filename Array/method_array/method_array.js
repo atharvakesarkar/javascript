72ee6a933b84ffc8fbe3e4e87b645780efe69652
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-20 18:58:40 +0530

    Add files via upload

---
+++ Array/method_array/method_array.js
@@ -0,0 +1,26 @@
+// PUSH - add elements to the end
+
+// let arr = [10,30,40,50,60];
+// arr.push(10);
+// console.log(arr);  //OUTPUT - (6) [10, 30, 40, 50, 60, 10]
+
+
+// UNSHIFT - add elements at the start
+
+// let arr = [10,20,30,40,50];
+// arr.unshift(200);
+// console.log(arr);  //OUTPUT - (6) [200, 10, 20, 30, 40, 50]
+
+
+// SHIFT - Delete at the start and return it
+
+// let arr = [10,20,30,40];
+// arr.shift();
+// console.log(arr);  //OUTPUT - (3) [20, 30, 40]
+
+
+// POP - Delete from the end and return it 
+
+// let arr = [10,20,30,40,50];
+// arr.pop();
+// console.log(arr); //OUTPUT - (4) [10, 20, 30, 40]
