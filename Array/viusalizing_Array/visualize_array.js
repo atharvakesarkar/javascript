1f97b3fb1167009259305264bc437b623a036b0d
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-20 18:23:52 +0530

    Update visualize_array.js

--- Array/viusalizing_Array/visualize_array.js
+++ Array/viusalizing_Array/visualize_array.js
@@ -1,7 +1,30 @@
 // linear collection of things
 // array is object , even when you fo 'type of nums" it will be object
 
-let nums = [1,2,3,4,5,5,6];
-console.log(nums[3]);
-console.log(nums.length);
-console.log(typeof nums);
+// let nums = [1,2,3,4,5,5,6];
+// console.log(nums[3]);
+// console.log(nums.length);
+// console.log(typeof nums);
+
+// let person = ['atharva','krutika','aaradhya','niyati','siddii'];
+// console.log(typeof person);
+// console.log(person[1]);
+
+
+
+// WE CAN CREATE MIX ARRAY 
+
+// let person = ['athu','krutu', 16 , 98 ];
+// console.log(person[3])
+
+
+// WE CAN CREATE EMPTY ARRAY
+
+// let arr = [ ];
+// console.log(arr.length);
+
+
+// WE CAN USE MULTIPLE SQUARE BRACKET
+// so the output is 3 , first bracket choose krutika and second bracket choose 3 that is t
+// let arr = ["Atharva", "krutika"];
+// console.log(arr[1][3]);
