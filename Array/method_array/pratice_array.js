f085f68436c78cb268fdfc613ec13400213eeb6d
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-21 08:11:41 +0530

    Add files via upload

---
+++ Array/method_array/pratice_array.js
@@ -0,0 +1,8 @@
+// qs. for the given start of the array, change it to the final form using  methods
+// start: ['january' , 'july', 'march' , 'august']
+
+// let arr = ['january' , 'july', 'march' , 'august'];
+// arr.shift();
+// arr.shift();
+// arr.unshift('june');
+// arr.unshift('july');
