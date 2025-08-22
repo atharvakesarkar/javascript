330fbed091f88603dab3d11dd0fc8f2b40cf4ad0
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-22 06:37:11 +0530

    Update method_array.js

--- Array/method_array/method_array.js
+++ Array/method_array/method_array.js
@@ -38,3 +38,50 @@
 // let arr = [10,20,30,40,50];
 // console.log(arr.includes(10)); // OUTPUT - true
 // console.log(arr.includes(60)); // OUTPUT - false
+
+
+// CONCAT - Use to merge the array       [IT RETURNS IN THE NEW ARRAY]
+
+// let arr1 = [1,2,3,4,5,6];
+// let arr2 = [10,20,30,40,50,60];
+// let all_arr = arr1.concat(arr2);
+
+// console.log(all_arr); //OUTPUT - (12) [1, 2, 3, 4, 5, 6, 10, 20, 30, 40, 50, 60]
+
+// console.log(arr1.concat(arr2)); //OUTPUT - (12) [1, 2, 3, 4, 5, 6, 10, 20, 30, 40, 50, 60]
+
+
+// REVERSE - reverse the elements
+// let arr1 = [10,20,30,40,50];
+// console.log(arr1.reverse());
+
+
+// SLICE - copies a portion of the array
+
+// let arr1 = [10,20,30,40,50,60,70];
+// console.log(arr1.slice());  //OUTPUT - (7) [10, 20, 30, 40, 50, 60, 70]
+// console.log(arr1.slice(2));  //OUTPUT - (5) [30, 40, 50, 60, 70]
+// console.log(arr1.slice(2,3));  //OUTPUT - [30]
+// console.log(arr1.slice(-2));  //OUTPUT - (2) [60, 70]
+
+
+// let arr = [10,30,40,50,60];
+// console.log(arr.slice(0,3)); //OUTPUT - (3) [10, 30, 40]
+
+
+
+// SPlICE - remove/replace/add element in place [MAKE THE CHANGES IN THE ORGINAL ARRAY]
+// splice(start, delete , item0...itemn );
+
+
+// arr = ["Atharva" , "Archie"];
+// arr.splice(1,1,"krutika","kanak","Ayush");
+// console.log(arr);    // OUTPUT - (4) ['Atharva', 'krutika', 'kanak', 'Ayush']
+
+
+// arr = ["BMW" , "AUDI" , "MERCEDES", "MARUTI", "HONDA"]
+// arr.splice(0,1,"Atharva");
+// console.log(arr)
+
+// arr.splice(2,1,"krutika");
+// console.log(arr);
