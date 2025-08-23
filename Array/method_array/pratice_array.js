c85fd992880593d13c7bc7c4c13ab2f9c2624b6a
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-23 19:45:16 +0530

    Refactor comments and reverse array in pratice_array.js

--- Array/method_array/pratice_array.js
+++ Array/method_array/pratice_array.js
@@ -6,3 +6,16 @@
 // arr.shift();
 // arr.unshift('june');
 // arr.unshift('july');
+
+
+
+// qs.2 start ['january' , 'july' , 'march' , 'august']
+
+// let arr = ['january', 'july','march', 'august']
+// console.log(arr.splice(0,2,'july','june'));  // OUTPUT - (2) ['january', 'july']
+// console.log(arr)  // OUTPUT - (4) ['july', 'june', 'march', 'august']
+
+
+let arr = ['c','c++','html', 'javascript', 'python', 'java','c#','sql']
+arr.reverse();
+console.log(arr.indexOf('javascript'));
