7436036f1af34fd8589b28d9734762de0b2a675f
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-23 20:20:58 +0530

    Add files via upload

---
+++ Array/Array_reference/reference.js
@@ -0,0 +1,11 @@
+// let num = [1,2,3,4,5];  //num is the reference varible as it has all the address of the values
+// console.log(num);
+
+
+// let arr1 = [1];  //the value is same but both the has differenct address becasue they are store in the different array
+// let arr2 = [1];
+// arr1 == arr2;
+
+// let arr = [1,2,3];
+// let arrcopy = arr; //here you can see as i copy my arr to the arrcopy the value and their address are also copy
+// arr == arrcopy //true
