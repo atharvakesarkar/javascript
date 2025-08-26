494ac29f5c9a4ca189d53027e37bd4c5cf3fe074
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-26 16:57:44 +0530

    Add files via upload

---
+++ loops/for_loops/for.js
@@ -0,0 +1,20 @@
+// the work that i need to multiple time , that can be done using loop
+
+// loops - to iterate[repeat] the picece of code 
+
+// Syntax - for(intilization; condition; updation; ){
+//                console.log(i);
+//       }
+
+
+// Forward loop
+// for (let i=1; i<=5; i++) {
+//     console.log(i);
+// }
+
+
+
+// Backward loop
+// for (let i=10; i>=1; i--){
+//     console.log(i);
+// }
