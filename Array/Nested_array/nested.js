b464adf6d4648ec2adbb8d80163d2e9a2326e2a6
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-26 13:11:35 +0530

    Add files via upload

---
+++ Array/Nested_array/nested.js
@@ -0,0 +1,8 @@
+// nesting means adding similar thing in to the one
+
+
+// Nested array = Multidimensioal array
+
+
+// let arr = [[1,2], [3,4], [5,6] , [7,8]];
+// console.log(arr);
---
+++ Array/Nested_array/pratice_nested.js
@@ -0,0 +1,53 @@
+// let tictac = [["x",null, "0"] , [null ,"x" ,null], ["0",null, "X"]];
+// tictac[0][1] = "O";
+
+// console.log(tictac);
+
+// PRATICE QUESTION ARRAY [PART 3]
+
+
+// let arr = [7,9,0,-2];
+// console.log(arr.slice(0,3));
+
+
+
+// let arr = [7,9,0,-2];
+// let n = -3;
+// console.log(arr.slice(n));
+
+
+
+// let str = "Atharva";
+// if (str == ""){
+//     console.log("The string is blank");
+// }else {
+//     console.log("string is not blank")
+// }
+
+
+
+// let str = "AtHARVA";
+// n = prompt("enter the index")
+// if ( str[n] == str[n].toLocaleLowerCase()){
+//     console.log("the character is in the lowercase")
+// } else {
+//     console.log("the charater is not in lowercase ")
+// }
+
+
+
+// let str = "  Atharva      ";
+// let trimstr = str.trim();
+// console.log(trimstr);
+
+
+
+
+// let arr = [10,20,30,40,50,60];  //Creating of an array
+// let n = prompt("enter the number you want to check: ") // taking input from the user to check the number
+// n = Number(n); //prompt always take string , so converting it in Number
+// if (arr.includes(n) == true){  //checking whether the number is in array or not , if include it will return true value
+//     console.log("YES, the element is in the array");
+// } else {
+//     console.log("NO , the element dosent exist") //else condition
+// }
