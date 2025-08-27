c60e7b2547576602150b798f503f439560e759f8
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-28 00:49:33 +0530

    Add files via upload

---
+++ loops/Nested_loop/nested.js
@@ -0,0 +1,6 @@
+// for (let i=1; i<=4; i++){   // outer loop
+//     console.log(`outer loop ${i}`);  // to show that inner loop will run 4 time becasue the outer loop condition is 4
+//     for (let j=1; j<=3; j++){  // inner loop
+//         console.log(j);
+//     }
+// }
