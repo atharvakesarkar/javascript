9293833e15fc72ff023d58afcbf3d99d05eef1f9
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-28 01:13:16 +0530

    Add files via upload

---
+++ loops/while_loops/while.js
@@ -0,0 +1,27 @@
+// forward while loop
+// let i = 1;
+
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// backward while loop
+
+// let i = 10;
+
+// while (i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+
+// pratice o to 20 print 
+
+// let i = 0;
+
+// while (i<=20){
+//     console.log(i);
+//     i = i+2;
+// }
