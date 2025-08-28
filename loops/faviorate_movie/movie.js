65231ee15aa3493ede4585fdff854444c1bf1108
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-08-28 12:16:41 +0530

    Update movie.js

--- loops/faviorate_movie/movie.js
+++ loops/faviorate_movie/movie.js
@@ -11,4 +11,26 @@ if (guess == favi_movie){
     console.log("wowwww! you are right");
 } else{
     console.log("you Quit");
+
 }
+
+
+
+// using break statement for Quit
+let favi_movie = "avatar";
+
+let guess = prompt("guess my faviorate movie name..");
+
+
+while((guess != favi_movie)){
+    if (guess == "Quit"){
+        break;
+    }
+    guess = prompt("oops! wrong. try again");
+}
+
+if (guess == favi_movie){
+    console.log("wowwww! you are right");
+} else{
+    console.log("you Quit");
+} 
