4cab847d566d3bfbbb876518425669a97ce4a40b
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-01 21:31:02 +0530

    Add files via upload

---
+++ loops/nested_Array_loop/nested_Array.js
@@ -0,0 +1,19 @@
+// nested_arr = [[10,20,30,40], [1,2,3,4]];
+
+// for (let i=0; i<nested_arr.length; i++){
+//     for(let j=0; j<nested_arr[i].length; j++){
+//         console.log(nested_arr[i][j]);
+//     }
+// }
+
+
+
+// quesiton
+// let arr = [["atharva",80], ["krutika" , 90], ["Archie", 95]];
+
+
+// for (let i=0; i<arr.length; i++){
+//     for(let j=0; j<arr[i].length; j++){
+//         console.log(arr[i][j]);
+//     }
+// }
