6628d2b5740be60d55f2a60484b7a27a4ace8519
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-02 00:41:26 +0530

    Add files via upload

---
+++ loops/for_of/of_for.js
@@ -0,0 +1,12 @@
+// let fruits = ["apple","bannana","cherry","lemon"];
+
+// for (i of fruits){
+//     console.log(i);
+// }
+
+
+let name = "Atharva kesarkar";
+
+for (i of name){
+    console.log(i);
+}
