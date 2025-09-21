73e2aa04b8caf7da14e2e9630e07dd02b91c8eda
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-21 10:51:12 +0530

    Add files via upload

---
+++ object literals/object_literal.js
@@ -0,0 +1,33 @@
+// MAKING OF OBJECT LIERALS
+
+// let student = {
+//     name: "Atharva",
+//     age: 18,
+//     rollno: 18,
+//     contactno: 9820919318 
+// };
+
+// console.log(student)
+
+
+
+// let item = {
+//     price: 100.00,
+//     discount: 20,
+//     color: ["red", "pink"]
+// };
+// console.log(item);
+
+
+// const twitter = {
+//     username: "Atharva kesarkar",
+//     content: "learn javascript in easy way",
+//     likes: 45,
+//     reposts: 24,
+//     tags: ["kesarkaratharva" , "siddhikaloji" , "Archiethakare" , "krutikathakare" , "shyamsunderkadam", "malinibhaskar"]
+// };
+
+// console.log(twitter);
+
+
+// ACCESSING THE VALUES
