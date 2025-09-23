db500268554f6cbb08e21f43a4f123ac8d456a0f
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-23 23:37:07 +0530

    Add files via upload

---
+++ object literals/nested_object_literals/nested_object.js
@@ -0,0 +1,34 @@
+const classinfo = {
+    Atharva : {
+        age: 18,
+        marks: 70
+    },
+
+    krutika : {
+        age: 18,
+        marks: 94
+    },
+
+    Archi : {
+        age: 17,
+        marks: 85
+    }, 
+
+    Bhumika: {
+        age: 18,
+        marks: 64
+    }
+};
+
+// console.log(classinfo);
+
+// console.log(classinfo.Archi); //to get all key value from object name as Archi
+// console.log(classinfo.Archi.marks); //to get specific value from the object archi , key marks
+
+// delete classinfo.Atharva.marks; //to delete the specific key - value from the object
+// console.log(classinfo.Atharva);  
+
+// classinfo.Atharva.grade = "A+";  //to add the new value 
+// console.log(classinfo.Atharva); 
+
+
