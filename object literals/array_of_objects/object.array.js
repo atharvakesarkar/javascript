8a128d2ea6561f8a30d3f2c92fc85f009786c811
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-25 12:16:00 +0530

    Add files via upload

---
+++ object literals/array_of_objects/object.array.js
@@ -0,0 +1,32 @@
+let student = [
+
+    {
+
+        name: 'Atharva',
+        age: 18,
+        contact: 9820919318,
+        education: "Bvoc"
+
+    },
+
+    {
+        name: "krutika",
+        age: 18,
+        contact: 9821110135,
+        education: "BAF"
+    },
+
+    {
+        name: "Archie",
+        age: 17,
+        contact: 9870219318,
+        education: "Bcom"
+    }
+
+];
+
+console.log(student[0]); //use to print all the element is array object
+
+console.log(student[1].name) //use to print specfic element
+
+console.log(student[1].gender = "Female");
