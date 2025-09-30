7487c8fa4c1e32227b8311efe5d6714dcad1d29f
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-30 19:57:58 +0530

    just add more code

--- object literals/array_of_objects/object.array.js
+++ object literals/array_of_objects/object.array.js
@@ -1,32 +1,62 @@
-let student = [
+// // let student = [
 
-    {
+// //     {
 
-        name: 'Atharva',
-        age: 18,
-        contact: 9820919318,
-        education: "Bvoc"
+// //         name: 'Atharva',
+// //         age: 18,
+// //         contact: 9820919318,
+// //         education: "Bvoc"
 
-    },
+// //     },
 
-    {
-        name: "krutika",
-        age: 18,
-        contact: 9821110135,
-        education: "BAF"
-    },
+// //     {
+// //         name: "krutika",
+// //         age: 18,
+// //         contact: 9821110135,
+// //         education: "BAF"
+// //     },   
 
-    {
-        name: "Archie",
-        age: 17,
-        contact: 9870219318,
-        education: "Bcom"
-    }
+// //     {
+// //         name: "Archie",
+// //         age: 17,
+// //         contact: 9870219318,
+// //         education: "Bcom"
+// //     }
 
-];
+// // ];
 
-console.log(student[0]); //use to print all the element is array object
+// // console.log(student[0]); //use to print all the element is array object
 
-console.log(student[1].name) //use to print specfic element
+// // console.log(student[1].name) //use to print specfic element
 
-console.log(student[1].gender = "Female");
+// // console.log(student[1].gender = "Female");
+
+
+
+
+// let student = [
+
+//     student1 = {
+//         firstname : "Atharva",
+//         middlename : "Rajesh",
+//         lastname : "kesarkar"
+//     },
+
+//     student2 = {
+//         firstname : "Archie",
+//         Middlename : " ",
+//         lastname : "Thakare"
+//     },
+
+//     student3 = {
+//         firstname : "Krutika",
+//         middlename : "Prakash",
+//         lastname : "Thakare"
+
+
+//     }
+// ];
+
+
+// console.log(student[2].husbandname = "Atharva");
+// console.log(student[2]);
