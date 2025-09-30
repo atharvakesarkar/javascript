267cb4fab2546fc7459aa571f7da5f2f9e541e69
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-09-30 21:23:03 +0530

    just added the random file into it

---
+++ object literals/Math object/math.js
@@ -0,0 +1,20 @@
+
+
+// absoulte value - the number is written always in postive number
+// console.log(Math.abs(-123)); //output - 123
+
+
+// it use to get power - we alsp use number ** number
+// console.log(Math.pow(2,10)); //1024
+
+
+// it is use for round of the number , it will give less than or equal than value
+// console.log(Math.floor(11));
+// console.log(Math.floor(-12.5));
+
+
+// ceil it is use to round of the number but it give the largest value
+// console.log(Math.ceil(-12.5));
+
+// it gives the random value between 0 to 1
+// console.log(Math.random()); 
