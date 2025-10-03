d45365ddff0fdbc5746b26c623d59c1e7f77d5d8
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-03 10:53:56 +0530

    Update guesss.js

--- object literals/Range integers/guessing/guesss.js
+++ object literals/Range integers/guessing/guesss.js
@@ -1,8 +1,25 @@
-// user enter a max number and then tries  to guess  a random genreated number between 1 to max
+// // user enter a max number and then tries  to guess  a random genreated number between 1 to max
 
+let user = prompt("enter your number: ");
 
-let user = prompt("enter the max value: ");
 
 let guess = Math.floor(Math.random() * user + 1);
-console.log(guess);
+// console.log(guess);
+
+while(true){
+    if (user == "quit"){
+        console.log("your guess is correct: ",guess);
+        break;
+        
+    }
+    if (guess != user){
+        guess = prompt("guess is incorrect! try again");
+    } else {
+        console.log("you are correct!!" , guess);
+        break;
+
+    }
+}
+
+
 
