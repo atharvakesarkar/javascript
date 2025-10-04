4d42a5a49c3c6179ece38f48f87c0dbe546e7cd9
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-04 10:44:15 +0530

    this is the pratice question that i have solve

---
+++ object literals/Range integers/guessing/pratice questions/pratice_range.js
@@ -0,0 +1,31 @@
+// q1. create a program that generates a random number respresnting a dice roll [the number should be written 1 to 6]
+
+// dice = Math.floor(Math.random() * 6 + 1);
+
+// console.log(dice);
+
+
+// q2. create an object  respresnting a car that stores the following properties for the car:name , model , color... print the car name.
+
+// let car = {
+//     name : "volkswagon",
+//     model: "v5",
+//     color: "sky blue"
+// }
+
+
+// console.log(car.name);
+
+
+
+// q3. create a object person with their name, age and city.. edit the  city orginal value to change it to "New york".. add a new property country and set it to the united states..
+
+// person = {
+//     name: "Atharva",
+//     age: 18,
+//     city: "Mumbai"
+// };
+
+// let change = person.city = "New york";
+
+// console.log(person)
