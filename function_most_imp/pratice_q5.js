99812625513837d1671b7b766650dd06944cd273
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-08 19:15:24 +0530

    Add files via upload

---
+++ function_most_imp/pratice_q5.js
@@ -0,0 +1,33 @@
+// create a function that returns the  sum of number from 1 to n
+
+
+// function loop(n){
+//     let sum = 0;
+//     for(let i=1; i<=n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// };
+
+// console.log(loop(2));
+
+
+// Start with a variable sum = 0 → ye total store karega.
+
+// Use a loop from 1 to n.
+    
+// Har step me current number ko sum me add karte jao.
+
+// Example:
+
+// sum = 0 + 1 → 1
+
+// sum = 1 + 2 → 3
+
+// sum = 3 + 3 → 6
+
+// sum = 6 + 4 → 10
+
+// sum = 10 + 5 → 15
+
+// Jab loop khatam ho jaye, return sum.   
