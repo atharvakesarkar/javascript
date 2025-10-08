a6d00dc4b8cf267da0de78ebfee2d89c506623d2
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-08 19:16:42 +0530

    just add more code and differnt style of using return keyword

--- function_most_imp/return_statement/return_js.js
+++ function_most_imp/return_statement/return_js.js
@@ -1,18 +1,60 @@
+// USE OF RETURN STATEMENT
 // function sum(a , b){
 //     return a+b
 // };
-
-
 // let part = sum(3,4);
 // console.log(part); //in js , a function's return statement sends value back to where the funciton was called, but it doesn't display it or returned the value, you typically  store it in a variable and then use console.log() or any other output method to display it
 
 
-// calulating 3 number sum
 
-function sum(a , b){
 
-    return a+b
-};
 
-let soln = sum(sum(10,10),3);
-console.log(soln);
+// CALCULATION SUM OF 3
+// function sum(a , b){
+
+//     return a+b
+// };
+
+// let soln = sum(sum(10,10),3);
+// console.log(soln);
+
+
+
+
+
+// RETURN STATEMENT IS THE LAST LINE OF function
+// function sum(a , b){
+//     console.log("hello");
+//     console.log("hello");
+//     return a+b
+//     console.log("hello2");
+//     console.log("hello2");  //The return statement ends a function, so any code written after it won’t execute.
+// }
+
+// console.log(sum(20,20)); //hello
+                            //hello
+                            //40
+
+
+
+
+// PRATICE 
+// function isadult(age){
+//     if (age >= 18){
+//         return "adult"
+//     } else {
+//         return "teenager"
+//     }
+//     console.log("bye bye") //after return it can't be execute again
+// }
+
+// console.log(isadult(8));
+
+
+
+
+
+
+
+
+                        
