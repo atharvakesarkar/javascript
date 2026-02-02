738874ccd78f3c092d06581f12a429b033a776b5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-02 22:09:23 +0530

    revision
    doing revision becasue of  gap , before learning new concepts

---
+++ revision/revision_js.js
@@ -0,0 +1,135 @@
+//using print statement
+// console.log("Atharva kesarkar");
+
+// -------------------------------------------------------------------------------------------------------------
+// creating the variable
+
+// let a = 20;
+// console.log(a);
+
+//assigning a function
+// let a;
+// a = 45;
+
+// console.log(a);
+
+// -------------------------------------------------------------------------------------------------------------
+//data types
+//typeof() - this function tell the datatype
+
+// let a = "15";
+// console.log(typeof a);
+
+// let b = true; //for boolean value
+// console.log(b);
+
+// let c = undefined; //example for undefined
+// console.log(c);
+
+// let d = null; //example for null
+// console.log(d);
+
+//-----------------------------------------------------------------------------------------------------------------
+//operations
+
+// num1 = 10;
+// num2 = 20;
+// add = num1 + num2;
+// mult = num1 * num2;
+// console.log(sum);
+// console.log(mult);
+
+// //-----------------------------------------------------------------------------------------------------------------
+// num1 = 0;
+// num2 = 0;
+
+// console.log(num1 / num2); //output -  NAN
+//NAN - not a number, represent the invalid value
+
+//opertor precidence - in this first () -> ** -> *,/,% -> + , -
+
+// console.log(2 ** 2 + 10 + 10 * 2);
+
+//-----------------------------------------------------------------------------------------------------------------
+//assignmet opertor
+
+// let num1 = 10;
+// let num2 = num1; // = -> equal is a assign opertor use for the assigning value
+// console.log(num2);
+
+// let age = prompt("enter your age: ");
+
+// if (age >= 18) {
+//   console.log("you are elgible for voting");
+// } else {
+//   console.log("you are not eligible for voting");
+// }
+
+//-----------------------------------------------------------------------------------------------------------------
+//unary opertor is the opertor that uses only one operant
+// age = 18;
+// output - 18
+// age++;
+// output - 18
+// age;
+// output - 19
+// age = 18;
+// output - 18
+// ++age;
+// output - 19
+
+//-----------------------------------------------------------------------------------------------------------------
+//string - collection of characeter
+
+// let name = "Atharva kesarkar";
+// console.log(name[0]); //this is called index , indexing
+
+//length function use for getting the length of the string
+
+// let name = "Atharva";
+// console.log(name.length);
+
+// ___________________________________________________________________
+
+//string concetenation
+
+// let firstname = "Atharva";
+// let lastname = "kesarkar";
+
+// console.log(firstname + lastname);
+// console.log(firstname, lastname);
+// console.log(firstname, "", lastname);
+
+//----------------------------------------------------------------------------------------------------------------
+
+//template literals - we use it when we want to add a value of vairable in the string
+
+// let name = prompt("enter your name: ");
+// console.log(`hello, ${name}.`);
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// conditional statements
+// let marks = prompt("enter your marks: ");
+
+// if (marks >= 100) {
+//   console.log("excellent");
+// } else if (marks >= 80) {
+//   console.log("very good");
+// } else if (marks >= 70) {
+//   console.log("good");
+// } else if (marks >= 60) {
+//   console.log("passed");
+// } else {
+//   console.log("failed");
+// }
+
+//-----------------------------------------------------------------------------------------------------------------
+
+//logical opertor
+
+// console.log(10 == 10 && 20 > 5); //and opertor - because both condition are true
+// console.log(10 == 10 || 20 < 5); //or opertor - any one of the both condition should be true
+// console.log(10 > 10 && !(20 < 5)); //not  opertor - both conditons are false
+
+//-----------------------------------------------------------------------------------------------------------------
