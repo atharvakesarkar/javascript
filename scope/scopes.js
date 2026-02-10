5e9b1f616dcffcbdac3f5e453d4a391e74b0f3bc
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-10 21:07:12 +0530

    scope js file
    it consist of defination and example code of function , block and lexical scope and also some information about hositing

---
+++ scope/scopes.js
@@ -0,0 +1,78 @@
+// scope - determines the accessbility of variables , objects and functions from differnt parts of the code
+
+//----------------------------------------------------------------------------------------------------------------
+//types:
+//function scope : variables define inside a funciton are not accessible (visible) from outside the function
+
+// let sum = 67; //global scope - it can be access inside and outside the function also , everywhere in the code
+
+// function calu(a, b) {
+//   let sum = a + b; //function scope
+//   console.log(sum); //print the function scope
+// }
+
+// calu(10, 20);
+
+// console.log(sum); //print the global scope
+
+//----------------------------------------------------------------------------------------------------------------
+//block scope : variables declared inside a {} block cannot be accessed outside the block
+
+// {
+//   let a = 10;
+//   console.log(a);
+// }
+// console.log(a);
+
+// ________________________________________________________
+// eg of block scope
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i); //i can be only access inside the block {}
+// }
+
+// console.log(i); //can't access i outside the block is called blocked scope
+
+//-----------------------------------------------------------------------------------------------------------------
+//lexical scope : a variable defined outside a function can be accessible inside anouther function degined after the variable declaration
+
+// function outside() {
+//   let a = 10;
+//   let b = 20;
+
+//   function inside() {
+//     console.log(a + b);
+//   }
+//   inside();
+// }
+
+// ______________________________________________________________________________________
+
+// Hoisting : in javascript  behaviour when the variable and function declarations are processed before execution, allowing them to be referenced earlier in the code
+
+// Function declarations are hoisted with their definition,
+// so they can be called before they appear in the code.
+
+// say();
+// function say() {
+//   console.log("hello guys");
+// }
+
+// ______________________________________________________________________________________
+
+// let variables are not accessible before initialization.
+// Accessing them earlier causes a ReferenceError due to
+// the Temporal Dead Zone.
+
+// console.log(a); // ReferenceError
+// let a = 10;
+
+//THE opposite is not possible
+// function outerfunc() {
+//   function innerfunc() {
+//     let a = 10;
+//     console.log(a);
+//   }
+
+//   innerfunc();
+// }
