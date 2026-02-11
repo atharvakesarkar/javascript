f9b4911058cd3424fab9e6b541aa3c70138175e5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-11 20:18:11 +0530

    this folder consist about function expression
    another way to make a function  and stored it into a variable also called as nameless function

---
+++ function_most_imp/function_expression/expression_func.js
@@ -0,0 +1,16 @@
+//funciton expression is a another way of defining function in this we define function inside a varible
+//and to call the function we use variable name
+
+//they are also called as nameless function
+const sum = function (a, b) {
+  //output on console - sum(10,10)
+  return a + b;
+};
+
+//
+const name = function () {
+  console.log("Athara kesarkar"); //outpur on console - name();
+};
+
+//IMPORTANT
+// If I want a function to behave like a variable and do everything a variable can do, I will use a function expression.
