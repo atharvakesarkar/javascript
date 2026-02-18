513c2cb7feee52d3190ecec15791697495aab051
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-18 22:02:36 +0530

    just a small code about methods

---
+++ methods/methods.js
@@ -0,0 +1,37 @@
+//when we can to define function for the object is called as methods
+
+// const calculator = {
+//   add: function (a, b) {
+//     return a + b;
+//   },
+
+//   sub: function (a, b) {
+//     return a - b;
+//   },
+
+//   mul: function (a, b) {
+//     return a * b;
+//   },
+// };
+
+// console.log(calculator.add(10, 10)); //output - 20 , here we will call the function use dot opertor
+
+//-----------------------------------------------------------------------------------------------------------
+
+//another way to create a method [shorthand - shortcut]
+
+// const calculator = {
+//   add(x, y) {
+//     return x + y;
+//   },
+
+//   sub(x, y) {
+//     return x - y;
+//   },
+
+//   mul(x, y) {
+//     return x * y;
+//   },
+// };
+
+// console.log(calculator.sub(10, 10));
