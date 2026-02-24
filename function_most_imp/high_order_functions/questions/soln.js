3a9ea1f58522410568709d1b22c9e76abaab7e2c
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-25 00:09:50 +0530

    added the code
    solve first question

---
+++ function_most_imp/high_order_functions/questions/soln.js
@@ -0,0 +1,34 @@
+// q1. write a javascript function that returns elements larger thana number
+
+// myarr = [10, 20, 30, 40, 50, 60];
+
+// // console.log(typeof myarr);
+// // console.log(myarr.length);
+
+// let app = function (num) {
+//   let result = [];
+//   for (let i = 0; i < myarr.length; i++) {
+//     //   console.log(myarr[i]);
+
+//     if (myarr[i] > num) {
+//       result.push(myarr[i]);
+//     }
+//   }
+//   return result;
+// };
+
+// let num = Number(prompt("enter a number: "));
+// console.log(app(num));
+
+//---------------------------------------------------------------------------------------------------------------
+
+//q2. write a javascript function  to extract unique characters from string
+// example: str = "abcdabcdefgggh"
+//          ans = "abcdefgh"
+
+let str = "abcdabcdefgggh";
+// console.log(str[1]);
+
+for (let i = 0; i < str.length; i++) {
+  // console.log(str[i]);
+}
