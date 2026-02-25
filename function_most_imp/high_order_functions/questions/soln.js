a4abc261ca9c7e2772f9d212a549549ee6f3b7e5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-25 22:39:01 +0530

    Uncommented JavaScript functions for various tasks

--- function_most_imp/high_order_functions/questions/soln.js
+++ function_most_imp/high_order_functions/questions/soln.js
@@ -26,9 +26,56 @@
 // example: str = "abcdabcdefgggh"
 //          ans = "abcdefgh"
 
-let str = "abcdabcdefgggh";
-// console.log(str[1]);
+// // console.log(str[1]);
 
-for (let i = 0; i < str.length; i++) {
-  // console.log(str[i]);
+// function getunique(str) {
+//   let unique = " ";
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (!unique.includes(str[i])) {
+//       unique += str[i];
+//     }
+//   }
+//   return unique;
+// }
+
+// console.log(getunique("abcdabcdefgggh"));
+
+//--------------------------------------------------------------------------------------------------------------
+
+// q3. write a javascript function that accept a list of country names as input and return the longest country name as the ouput
+
+// eg = input = ["austrila" , "india", "united states of america"]
+//      output - "united states of america"
+
+// let obj = ["America", "india", "united states of america"];
+
+// function highest(obj) {
+//   let longest = " ";
+
+//   for (let i = 0; i < obj.length; i++) {
+//     // console.log(obj[i]);
+//     if (obj[i].length > longest.length) {
+//       longest = obj[i];
+//     }
+//   }
+//   return longest;
+// }
+
+// console.log(highest(obj));
+
+//q4. write a javascript function to count  the number of vowels in string argument
+
+let name = "Atharva kesarkar";
+
+let count = "";
+
+for (let i = 0; i < name.length; i++) {
+  // console.log(name[i]);
+}
+
+let vowels = "aeiou";
+
+for (let n = 0; n < vowels.length; n++) {
+  console.log(vowels[n]);
 }
