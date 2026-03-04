c3ceff30bda711a7ebf18bd431d6a8a1f56779b7
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-04 22:00:23 +0530

    Add files via upload

---
+++ part_7/miscellnaous/script_misc.js
@@ -0,0 +1,39 @@
+// Arrow functions  - it is a another technique that is use to define function but also , it is use to reduce the line of code a normal define funciton usally occupied
+
+// SYNTAX - const func = (args1 , args2....) => {function defination}
+
+// code:
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// sum(10, 10);
+
+// const square = (n) => {
+//   return n * n;
+// };
+
+// console.log(square(2));
+
+//-------------------------------------------------------------------------------------------------------------
+
+// Array functions - implict return - means a function automatically returns a value without writing the return keyword
+
+// const sum = (a, b) => a * b;
+// console.log(sum(10, 10));
+
+//--------------------------------------------------------------------------------------------------------------
+// setTimeout = it is a build in function that is use to set a specfic time for execution of the function , it is milsecond - eg: 1000 = 1 second
+
+const name = () => {
+  console.log("Atharva kesarkar");
+};
+
+let time = setTimeout(name, 4000);
+// console.log(time);
+
+console.log("hello , my name is ");
+console.log("hello , my name is ");
+console.log("hello , my name is ");
+console.log("hello , my name is ");
