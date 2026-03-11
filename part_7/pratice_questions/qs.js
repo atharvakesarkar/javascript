3adff42d4180b2d9421ea366099f4e6228b809d4
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-11 21:41:26 +0530

    Add files via upload

---
+++ part_7/pratice_questions/qs.js
@@ -0,0 +1,58 @@
+// write a Arrow function that returns the square number of n
+
+// const square = (n) => n * n;
+
+// let n = prompt("enter a number: ");
+// console.log(square(n));
+
+//------------------------------------------------------------------------------------------------------------
+
+//Write a function that prints "hello world" 5 times at the intervals of 2 sec each
+
+// let print = function () {
+//   console.log("Hello world");
+// };
+
+// let id = setInterval(print, 2000);
+
+// console.log(id);
+
+// setTimeout(() => {
+//   clearInterval(1);
+// }, 10000);
+
+//-----------------------------------------------------------------------------------------------------------------
+// write a function that named  arrayAverage that accepts an array of numbers and returns the avrage of those numbers
+
+// const arrayAverage = (arr) => {
+//   let total = 0;
+
+//   for (let num of arr) {
+//     total += num;
+//   }
+
+//   return total / arr.length;
+// };
+
+// let arr = [10, 20, 30, 40, 50];
+
+// console.log(arrayAverage(arr));
+
+//----------------------------------------------------------------------------------------------------------------
+// write an Arrow function name as isEven() that takes a single number as argument and returns if it is even or not
+
+// const isEven = (num) => {
+//   if (num % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+
+// let num = Number(prompt("Enter a number:"));
+
+// console.log(isEven(num));
+
+//----------------------------------------------------------------------------------------------------------------
+
+//
