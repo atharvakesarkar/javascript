d881b5f8f75c4a5863f09eebf0482f62c2ca8f89
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-25 12:35:48 +0530

    Refactor comments on array methods in arraymethod.js
    Updated comments and added explanations for array methods.

--- Array/part8_arraymethods/arraymethod.js
+++ Array/part8_arraymethods/arraymethod.js
@@ -1,14 +1,14 @@
 // for each - it is used to pass the funciton tht will run on each element of the array
 // _____________________________________________________
 
-let myarr = [10, 20, 30, 40, 50];
+// let myarr = [10, 20, 30, 40, 50];
 
-function print(el) {
-  console.log(el * el);
-}
+// function print(el) {
+//   console.log(el * el);
+// }
 
-myarr.forEach(print);
-console.log(myarr);
+// myarr.forEach(print);
+// console.log(myarr);
 
 //-___________________________________________________________
 
@@ -40,12 +40,52 @@ console.log(myarr);
 //   console.log(student_details.name);
 // });
 
-// __________________________________________________________________
+//-----------------------------------------------------------------------------------------------------------------
+// map function - it is a transformer it takes the exisiting array  , runs a function every iteam and collect result into a new array of the exact same length
 
-// map function -
+// let arr = [1, 2, 3, 4, 5];
 
-let arr = [1, 2, 3, 4, 5];
+// let double = arr.map((el) => {});
 
-arr.map((el) => {
-  console.log(el);
-});
+// console.log(double);
+
+// _____________________________________________________
+// data from line no 22 , student array is used
+// let gpa = student.map((mark) => {
+//   return mark.marks / 10;
+// });
+
+// console.log(gpa);
+
+// -------------------------------------------------------------------------------------------------------------------
+// filter - create a new array filled by the elemetnt that pass a test provided by a function it acts as a gatekepper for your data
+
+// let myarr = [2, 4, 10, 34, 67, 23];
+
+// let even = myarr.filter((el) => {
+//   return el % 2 == 0;
+// });
+
+// console.log(even);
+
+//-------------------------------------------------------------------------------------------------------------------
+// Every - the method checks if every single element in array passes the test , if all the element are true it will give all the true values and even if one value is false all the false element are stored
+
+// let arr = [10, 20, 30, 40, 50, 60];
+
+// let newarr = arr.every((el) => {
+//   return el % 2 == 0;
+// });
+
+// console.log(newarr);
+
+//---------------------------------------------------------------------------------------------------------
+// some - if even one element passes your test , the where thing evaluated to true if every single one fails it evaluates false
+
+// let myarr = [15, 20, 30, 40, 50, 35];
+
+// let even = myarr.some((el) => {
+//   return el % 2 == 0;
+// });
+
+// console.log(even);
