efeeba4830e3f363991a0ec863bc19f31d750d56
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-04-05 12:11:04 +0530

    Update pratice_soln.js

--- Array/part8_arraymethods/pratice_soln.js
+++ Array/part8_arraymethods/pratice_soln.js
@@ -23,3 +23,22 @@ let min_el = myarr.reduce((min , el) => {
 
 // console.log(min_el);
 
+// ----------------------------------------------------------------------------------------------------------------
+// square and sum the array elements using the arrow funciton and then find the average of array
+
+
+let num = [2,4,6,8,10,12];
+
+let square = num.map((el) => {
+    return el * el;
+});
+
+let add = square.reduce((arg , el) => {
+    return arg + el;
+}, 0);
+
+let avg = add / square.length;
+
+console.log(square);
+console.log(add);
+console.log(avg);
