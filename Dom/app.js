94082d0e0198188c7c099f6546a226048c6881a3
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-04-09 17:15:28 +0530

    Update app.js

--- Dom/app.js
+++ Dom/app.js
@@ -34,3 +34,33 @@ for (let i = 0;  i < smallimages.length; i++){
 let all_para = document.getElementsByTagName("p");
 // console.dir(all_para[0]);
 // console.dir(all_para[0].textContent = "Atharva kesarkar");
+
+// ----------------------------------------------------------------------------------------------------------------
+// Query selector - it select and return the first element in the document that matches a specified css selector and returns it in an object 
+
+// using tagname
+let h1_obj = document.querySelector("h1");
+// console.dir(h1_obj);
+// console.log(h1_obj);
+
+// using the id 
+let p_obj = document.querySelector("#description");
+// console.log(p_obj);
+// console.dir(p_obj);
+
+// using class name
+let images_obj = document.querySelector(".images");
+// console.dir(images_obj);
+// console.log(images_obj);
+
+// selects the first <li> inside a <ul> within a <div> using the css descendant selector
+// descendant selector - a CSS selector that targets elements nested inside other elements , seperated by spaces 
+let use_descedant_op = document.querySelector("div ul li");
+// console.dir(use_descedant_op);  
+
+// ________________________________________________________________________________________________________________
+// querySelectorAll() - selects all elements that match a specified css selector and returns them in a static NodeList
+// NodeList - it is an object that represents a collection of DOM nodes  
+
+let all_a_tag = document.querySelectorAll("a");
+// console.dir(all_a_tag);
